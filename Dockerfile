8f9c0c4a8a41e530a64eb0b94cf22cfa040fb7da
Retzudo <christoph.willinger@gmail.com>
2017-04-03 11:57:22 +0200

    Modernisierung
    - Einsatz von "Schluck" (gulp)
    - Umstrukturierung --> Quellen/Verteilung
    - Wegfall von HAML -- neumoderner Mumpitz!
    - Neue Kai-Datei ohne Caddy (Dockerfile)

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM nginx:alpine
+
+COPY verteilung /usr/share/nginx/html
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+gulp = require('gulp');
+sass = require('gulp-sass');
+uglify = require('gulp-uglify');
+
+gulp.task('html', function () {
+return gulp.src('./quellen/html/**/*.html')
+  .pipe(gulp.dest('./verteilung'))
+});
+
+gulp.task('css', function () {
+  return gulp.src('./quellen/sass/**/*.sass')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./verteilung/stilblaetter'));
+});
+
+gulp.task('js', function () {
+  return gulp.src('./quellen/js/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./verteilung/javaskripte'))
+});
+
+gulp.task('default', ['html', 'css', 'js']);
